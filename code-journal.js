0d8f250d101ea033d7418d45d72ddb2ec3e966a2
katie <katie01bug05@gmail.com>
2021-03-09 11:58:39 -0700

    2/25CodeJournalThroughLine84

--- code-journal.js
+++ code-journal.js
@@ -35,4 +35,51 @@ let myBool = true
 
 let myArray = [] // this is an empty array
 
+//              0     1      2       3      4   
 let myArray2 = [42, "Bob", myBool, ANSWER, true]
+
+let secondElement = myArray2[1];
+
+let lastItem = myArray2[myArray2.length - 1];
+
+myArray2.push("Thor"); // added an element to the end of myArray2
+
+myArray2.unshift("Hello World!");
+
+let myLongString = 
+    "hahahahahahahahahahahahahahahahahahahahahahahaha";
+
+myLongString.length;
+
+// Object
+
+let minObject = {};
+
+const myCar = {
+    make: "Tayota",
+    color: "Red",
+    year: "2005",
+    vin: "12345678910111213141516171819"
+};
+
+myCar.numDoors = 4;
+
+const anotherObject = {
+    wordz: ["foo", "bar", "baz"],
+    car: {
+        make: "Volkswagon",
+        model: "beetle"
+    },
+    awesome: true
+};
+
+// Functions
+
+function myFunction() {
+    return "My greeting to you...";
+}
+
+function sumTwoThings(one, two) {
+    return one + two;
+}
+//2/25
