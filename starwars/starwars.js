23a8ece2aecb1c9ea458e7a4841e5d1ccb570819
katie <katie01bug05@gmail.com>
2021-03-16 22:49:51 -0600

    StarWarsBase

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -1,7 +1,29 @@
 import { films } from '../data/films.js'
 
-let itemOne = document.querySelector('#item1');
+/*let itemOne = document.querySelector('#item1');
+let itemTwo = document.querySelector('#item2')
 
 itemOne.textContent = films[2].title;
+itemTwo.textContent = films[1].title*/
 
-console.log(films[0].title);
+//console.log(films[0].title);
+
+let movieList = document.querySelector('.movieList')
+
+for (let i = 0; i < films.length; i++) {
+    const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1))
+
+    let newImage = document.createElement('img')
+    //newImage.src
+    newItem.textContent = foundFilm.title
+    titlelist.appendChild(newItem)
+
+
+    console.log(foundFilm)
+}
+
+function getLastNumber(url) {
+    let end = url[url.length - 2]
+    return parseInt (end, 10)
+}
+//3/2 start
