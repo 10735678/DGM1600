adc672ad5a49d10650143397a335ae7e05ef72db
katie <katie01bug05@gmail.com>
2021-03-25 09:32:40 -0600

    IllGetItEventually

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -1,11 +1,10 @@
-const message = 'Hello World'
-
-console.log(message)
-
 import { films } from '../data/films.js'
 
-let item1 = document.querySelector('#item1')
+let itemOne = document.querySelector('#item1')
 
-itemOne.textContent = films[0].title;
+itemOne.textContent = films[0].title
 
 console.log(films[0].title)
+
+
+
