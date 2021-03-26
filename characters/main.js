a2d42f4db9b451e0486bd39e9c48f376694178f3
katie <katie01bug05@gmail.com>
2021-03-25 23:03:35 -0600

    eventlistenerbutton

--- characters/main.js
+++ characters/main.js
@@ -2,7 +2,28 @@ import { people } from '../Data/people.js'
 
 const mainContent = document.querySelector('#main')
 
-people.forEach(person => {
+const mainHeader = document.createElement('header')
+
+const maleButton = document.createElement('button')
+maleButton.textContent = 'Male Characters'
+maleButton.addEventListener('click', () => {
+    populateDOM(maleCharacters)
+})
+
+mainHeader.appendChild(maleButton)
+document.body.insertBefore(mainHeader, mainContent)
+
+const maleCharacters = people.filter(person => person.gender === 'male')
+const femaleCharacters = people.filter(person => person.gender === 'female')
+const otherCharacters = people.filter(person => {
+    if (person.gender === 'n/a' || person.gender === 'none') {
+        return person
+    }
+})
+
+
+function populateDOM(characters) {
+    characters.forEach(person => {
     const charFigure = document.createElement('figure')
     const charImg = document.createElement('img')
     let charNum = getLastNumber(person.url)
@@ -14,7 +35,8 @@ people.forEach(person => {
     charFigure.appendChild(charImg)
     charFigure.appendChild(charCaption)
     mainContent.appendChild(charFigure)
-})
+    })
+}
 
 function getLastNumber(url) {
     let end = url.lastIndexOf('/')
