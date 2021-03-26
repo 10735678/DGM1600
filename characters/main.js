3cfdfe04c02181d74ede909d1aea314a64bfad43
katie <katie01bug05@gmail.com>
2021-03-25 21:57:51 -0600

    characterImages

--- characters/main.js
+++ characters/main.js
@@ -1,1 +1,26 @@
-console.log("Welcome to Characters!")
+import { people } from '../Data/people.js'
+
+const mainContent = document.querySelector('#main')
+
+people.forEach(person => {
+    const charFigure = document.createElement('figure')
+    const charImg = document.createElement('img')
+    let charNum = getLastNumber(person.url)
+    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+    const charCaption = document.createElement('figcaption')
+
+    charCaption.textContent = person.name
+
+    charFigure.appendChild(charImg)
+    charFigure.appendChild(charCaption)
+    mainContent.appendChild(charFigure)
+})
+
+function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if (url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)
+}
