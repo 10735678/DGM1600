ed54cebc8112d9bbe5d997353ee9d128b56c0e15
katie <katie01bug05@gmail.com>
2021-03-28 23:56:25 -0600

    removeChildren

--- starships/main.js
+++ starships/main.js
@@ -1,5 +1,5 @@
 import { starships } from '../Data/starships.js'
-import { getLastNumber } from '../utils/index.js'
+import { removeChildren, getLastNumber } from '../utils/index.js'
 
 const main = document.querySelector('main')
 const navList = document.querySelector('.navList')
@@ -22,11 +22,40 @@ function populateNav() {
 }
 
 function populateShipView(shipData) {
+    removeChildren(shipView)
     let shipImage = document.createElement('img')
     let shipNum = getLastNumber(shipData.url)
     shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+    shipImage.addEventListener('error', () => {
+        console.log('Sorry! Image not available')
+        shipImage.hidden = true
+    })
     shipView.appendChild(shipImage)
 }
 
 populateNav()
-populateShipView()
+
+function addStarField(element, numStars) {
+    element.style.setProperty('background-color', 'black')
+    for (let i = 0; i <numStars; i++) {
+        let star = document.createElement('div')
+        star.style.setProperty('width', '2px')
+        star.style.setProperty('height', '2px')
+        star.style.setProperty('background-color', 'white')
+        let xy = getRandomPosition()
+        star.style.left = `${xy[0]}px`
+        star.style.top = `${xy[1]}px`
+        star.style.setProperty('position', 'absolute')
+        element.appendChild(star)
+    }
+}
+
+function getTandomPosition() {
+    let y = document.body.scrollHeight
+    let x = document.body.scrollWidth
+    let randomY = Math.floor(Math.random() * y)
+    let randomX = Math.floor(Math.random() * x)
+    return [randomX, randomY]
+}
+
+addStarField(document.querySelector('body'), 1000)
