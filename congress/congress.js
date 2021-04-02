c366177d644548957cb05798316b21c765ce21eb
katie <katie01bug05@gmail.com>
2021-04-01 23:52:35 -0600

    ImportingandSettingUpforCongress

---
+++ congress/congress.js
@@ -0,0 +1,13 @@
+import {representatives} from '../Data/representatives.js'
+
+
+const congressGrid = document.querySelector('.congressGrid')
+const seniorityButton = document.querySelector('#seniorityButton')
+const birthdayButton = document.querySelector('#birthdayButton')
+
+function populateCongressDiv(somearray) {
+    somearray.forEach(person => {
+        let personDiv = document.createElement('div')
+        let personFig = document.createElement('figure')
+    })
+}
