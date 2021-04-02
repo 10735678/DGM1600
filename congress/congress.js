ceb0dbf408217ff024889c8140da9085f334fbb4
katie <katie01bug05@gmail.com>
2021-04-02 00:11:32 -0600

    figImage/captions

--- congress/congress.js
+++ congress/congress.js
@@ -9,5 +9,20 @@ function populateCongressDiv(somearray) {
     somearray.forEach(person => {
         let personDiv = document.createElement('div')
         let personFig = document.createElement('figure')
+        let figImg = document.createElement('img')
+        let figCaption = document = document.createElement('figcaption')
+
+        figCaption.textContent = person.name
     })
 }
+
+function getSimplifiedPeople(peopleList) {
+    return peopleList.map(person => {
+        return {
+            id: person.id,
+            name: person.first_name
+        }
+    })
+}
+
+populateCongressDiv(getSimplifiedPeople(representatives))
