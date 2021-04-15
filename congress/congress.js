ea6635f3a540e53f3e60b6eebd62c3833db9b423
katie <katie01bug05@gmail.com>
2021-04-14 23:19:20 -0600

    loadingPictures

--- congress/congress.js
+++ congress/congress.js
@@ -1,7 +1,44 @@
 import { senators } from '../data/senators.js'
 import { representatives } from '../Data/representatives.js'
 
-const repubButton = document.querySelector('#republicans')
+//console.log(senators.length, representatives.length)
+
+const congressGrid = document.querySelector('.congressGrid')
+const seniority = document.querySelector('#seniority')
+const birthday = document.querySelector('#birthday')
+
+function populateCongressDiv(simplifiedList) {
+    simplifiedList.forEach(person => {
+        let personDiv = document.createElement('div')
+        personDiv.className = 'figureDiv'
+        let personFig = document.createElement('figure')
+        let figImg = document.createElement('img')
+        let figCaption = document.createElement('figcaption')
+
+        figImg.src = person.imgURL
+        figCaption.textContent = person.name
+
+        personFig.appendChild(figImg)
+        personFig.appendChild(figCaption)
+        personDiv.appendChild(personFig)
+        congressGrid.appendChild(personDiv)
+    })
+}
+
+function getSimplifiedPeople(peopleList) {
+    return peopleList.map(person => {
+        let middleName = person.middle_name ? ` ${person.middle_name}` : ``
+        return {
+            id: person.id,
+            name: `${person.first_name}${middleName} ${person.last_name}`,
+            imgURL: `https://www.govtrack.us/static/legislator-photos/${person.govtrack_id}-200px.jpeg`
+        }
+    })
+}
+
+populateCongressDiv(getSimplifiedPeople(representatives))
+
+/*const repubButton = document.querySelector('#republicans')
 
 repubButton.addEventListener('click', () => {
     showRepublicans()
@@ -18,32 +55,4 @@ function showRepublicans() {
         return smallRepub
 })
     console.log(repubs)
-}
-
-//console.log(senators.length, representatives.length)
-
-/*const congressGrid = document.querySelector('.congressGrid')
-const seniorityButton = document.querySelector('#seniorityButton')
-const birthdayButton = document.querySelector('#birthdayButton')
-
-function populateCongressDiv(somearray) {
-    somearray.forEach(person => {
-        let personDiv = document.createElement('div')
-        let personFig = document.createElement('figure')
-        let figImg = document.createElement('img')
-        let figCaption = document.createElement('figcaption')
-
-        figCaption.textContent = person.name
-    })
-}
-
-function getSimplifiedPeople(peopleList) {
-    return peopleList.map(person => {
-        return {
-            id: person.id,
-            name: person.first_name
-        }
-    })
 }*/
-
-//populateCongressDiv(getSimplifiedPeople(representatives))
