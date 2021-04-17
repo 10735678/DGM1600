6d0c8d7f950cbed865dfebe5f605fef3182efaf9
katie <katie01bug05@gmail.com>
2021-04-16 23:52:50 -0600

    Start of Pokemon/css

---
+++ pokemon/pokecode.js
@@ -0,0 +1,18 @@
+const pokeGrid = document.querySelector('pokeGrid')
+const loadButton = document = document.querySelector('loadPokemon')
+
+loadButton.addEventListener('click', () => {
+    loadPage()
+})
+
+async function getAPIData(url) {
+    try {
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function loadPage() {
+
+}
