84e6af21ea7e2f273fe1a10a022906a250bcf4f2
katie <katie01bug05@gmail.com>
2021-04-22 10:55:54 -0600

    newbuttonsforPokemon, and start of card.

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,6 +1,7 @@
 const pokeGrid = document.querySelector('.pokeGrid')
 const loadButton = document.querySelector('.loadPokemon')
 
+
 loadButton.addEventListener('click', () => {
     loadPage()
 })
@@ -16,9 +17,36 @@ async function getAPIData(url) {
 }
 
 function loadPage() {
-    getAPIData(`https://pokeapi.co/api/v2/pokemon/7`).then(
+    getAPIData(`https://pokeapi.co/api/v2/pokemon/?limit=25`).then(
         (data) => {
-            console.log(data)
+            for (const singlePokemon of data.results) {
+            populatePokecard(singlePokemon)
         }
+    }
     )
+}
+
+function populatePokecard(singlePokemon) {
+    let pokeScene = document.createElement('div')
+    pokeScene.className = 'scene'
+    let pokeCard = document.createElement('div')
+    pokeCard.className = 'card'
+    
+    pokeCard.appendChild(populateCardFront(singlePokemon))
+
+
+
+    pokeScene.appendChild(pokeCard)
+    pokeGrid.appendChild(pokeScene)
+    console.log(singlePokemon)
+}
+
+function populateCardFront(pokemon) {
+    let pokeFront = document.createElement('div')
+    pokeFront.className = 'card_face card_face--front'
+    let frontLabel = document.createElement('p')
+    frontLabel.textContent = pokemon.name
+
+    pokeFront.appendChild(frontLabel)
+    return pokeFront
 }
