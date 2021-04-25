968400afe6adeaf8e8575a18761dd263603e17ea
katie <katie01bug05@gmail.com>
2021-04-25 08:41:00 -0600

    Trying to get the newbutton to work

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -9,33 +9,68 @@ loadButton.addEventListener('click', () => {
 })
 
 fetchButton.addEventListener('click', () => {
-    let pokeId = prompt("Pokemon Name:").toLocaleLowerCase()
-    console.log(pokeId);
-    getAPIData(`https://pokeapi.co/api/v2/pokemon/${pokeId}`).then(
-        data => populatePokecard(data)
-    ).catch(error => console.log(error))
-})
+    let pokeId = prompt('Pokemon ID').toLowerCase()
+    getAPIData(`https://pokeapi.co/api/v2/pokemon/${pokeId}`)
+      .then((data) => populatePokeCard(data))
+      .catch((error) => console.log(error))
+  })
 
-class Pokemon {
-    constructor(name, height, weight, abilities, moves) {
-        this.name = name
-        this.height = height
-        this.weight = weight
-        this.abilities = abilities
-        this.moves = moves
+  class Pokemon {
+    constructor(name, height, weight, abilities, moves, types, stats) {
+      this.id = 900
+      this.name = name
+      this.height = height
+      this.weight = weight
+      this.abilities = abilities
+      this.moves = moves
+      this.types = types
+      this.stats = stats
     }
-}
+  }
 
 
-newButton.addEventListener('click', () => {
-    let pokeName = prompt("Name your Pokemon!")
-    let pokeHeight = prompt("How tall are they?")
-    let pokeWeight = prompt("How much do they weigh?")
-    let pokeAbilities = prompt("Top abilities?")
-    let pokeMoves = prompt("Moves?")
-    let newPokemon = new Pokemon(pokeName, pokeHeight, pokeWeight, pokeAbilities, pokeMoves)
-    populatePokecard(newPokemon)
-})
+  newButton.addEventListener('click', () => {
+ 
+    let pokeName = prompt('Name your Pokemon')
+    let pokeHeight = prompt('Height?')
+    let pokeWeight = prompt('Weight?')
+    let pokeAbilities = prompt(
+      'What are your Pokemon abilities?')
+  
+    let abilitiesArray = getAbilitiesArray(pokeAbilities)
+    let newPokemon = new Pokemon(
+      pokeName,
+      234,
+      3000,
+      abilitiesArray,
+      ['study', 'code', 'silence'],
+      [
+        {
+          type: {
+            name: 'normal',
+          },
+        },
+      ],
+      [{
+        base_stat: 100,
+        stat: {
+          name: "hp"
+        }
+      }]
+    )
+    populatePokeCard(newPokemon)
+  })
+  
+  function getAbilitiesArray(commaString) {
+    let tempArray = commaString.split(',')
+    return tempArray.map((abilityName) => {
+      return {
+        ability: {
+          name: abilityName,
+        },
+      }
+    })
+  }
 
 async function getAPIData(url) {
     try {
@@ -47,47 +82,45 @@ async function getAPIData(url) {
     }
 }
 
-function loadPage() {
-    getAPIData(`https://pokeapi.co/api/v2/pokemon/?limit=100`).then(
-   async (data) => {
-            for (const singlePokemon of data.results) {
-                await getAPIData(singlePokemon.url).then(
-                    (pokeData) => populatePokecard(pokeData)
-                )
-            
-        }
-    }
-    )
-}
-
-function populatePokecard(singlePokemon) {
-    let pokeScene = document.createElement('div')
-    pokeScene.className = 'scene'
-    let pokeCard = document.createElement('div')
-    pokeCard.className = 'card'
-    pokeCard.addEventListener('click', () => {
-        pokeCard.classList.toggle('is-flipped')
+function loadPage(offset, limit) {
+    getAPIData(
+      `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${100}`,
+    ).then(async (data) => {
+      for (const singlePokemon of data.results) {
+        await getAPIData(singlePokemon.url).then((pokeData) =>
+          populatePokeCard(pokeData),
+        )
+      }
     })
-    
-    pokeCard.appendChild(populateCardFront(singlePokemon))
+  }
 
-    pokeCard.appendChild(populateCardBack(singlePokemon))
+function populatePokeCard(singlePokemon) {
 
-    pokeScene.appendChild(pokeCard)
-    pokeGrid.appendChild(pokeScene)
-    console.log(singlePokemon)
+  let pokeScene = document.createElement('div')
+  pokeScene.className = 'scene'
+  let pokeCard = document.createElement('div')
+  pokeCard.className = 'card'
+  pokeCard.addEventListener('click', () => {
+    pokeCard.classList.toggle('is-flipped')
+  })
+
+  pokeCard.appendChild(populateCardFront(singlePokemon))
+
+  pokeCard.appendChild(populateCardBack(singlePokemon))
+
+  pokeScene.appendChild(pokeCard)
+  //pokeGrid.appendChild(pokeScene)
 }
 
 function populateCardFront(pokemon) {
     console.log(pokemon)
     let pokeFront = document.createElement('div')
     pokeFront.className = 'card_face card_face-front'
-    /*let frontLabel = document.createElement('p')
-    frontLabel.textContent = pokemon.name*/
+
     let frontImage = document.createElement('img')
     frontImage.src = `images/${getImageFileName(pokemon)}.png`
     pokeFront.appendChild(frontImage)
-  //pokeFront.appendChild(frontLabel)
+
     
     return pokeFront
 }
@@ -106,7 +139,6 @@ function populateCardBack(pokemon) {
             backType.textContent = pokeType.type.name;
             pokeBack.appendChild(backType)
             pokeBack.classList.add(pokemon.types[0].type.name)
-            
         })
 
         return pokeBack
