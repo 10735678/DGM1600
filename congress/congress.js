a69fdf076dfd17aacae75aa451c92fd3b32fc25d
katie <katie01bug05@gmail.com>
2021-04-29 14:20:41 -0600

    republican/democratsButtons

--- congress/congress.js
+++ congress/congress.js
@@ -1,23 +1,47 @@
 import { senators } from '../data/senators.js'
-import { representatives } from '../Data/representatives.js'
-
-//console.log(senators.length, representatives.length)
+import { representatives } from '../data/representatives.js'
+import { removeChildren } from '../utils/index.js'
 
 const congressGrid = document.querySelector('.congressGrid')
-const seniority = document.querySelector('#seniority')
-const birthday = document.querySelector('#birthday')
+const seniorityButton = document.querySelector('#seniorityButton')
+const birthdayButton = document.querySelector('#birthdayButton')
+const republicansButton = document.querySelector('#republicans')
+const democratsButton = document.querySelector('#democrats')
+const missedVotes = document.querySelector('#missedVotes')
+
+republicansButton.addEventListener('click', () => {
+    populateCongressDiv(filterCongressPeople(representatives, 'R'))
+})
+
+democratsButton.addEventListener('click', () => {
+    populateCongressDiv(filterCongressPeople(representatives, 'D'))
+})
+
+
+missedVotes.addEventListener('click', () => {
+    console.log(missedVotesMember(senators))
+    //alert(missedVotesMember(senators))
+}
+)
+
+seniorityButton.addEventListener('click', () => senioritySort())
 
 function populateCongressDiv(simplifiedList) {
+    removeChildren(congressGrid)
     simplifiedList.forEach(person => {
         let personDiv = document.createElement('div')
         personDiv.className = 'figureDiv'
         let personFig = document.createElement('figure')
         let figImg = document.createElement('img')
         let figCaption = document.createElement('figcaption')
+        let partyIcon = document.createElement('i')
+        if (person.party === 'R') partyIcon.className = 'fas fa-republican'
+        if (person.party === 'D') partyIcon.className = 'fas fa-democrat'
+        if (person.party === 'ID') partyIcon.className = 'fas fa-mitten'
 
         figImg.src = person.imgURL
         figCaption.textContent = person.name
-
+        figCaption.appendChild(partyIcon)
         personFig.appendChild(figImg)
         personFig.appendChild(figCaption)
         personDiv.appendChild(personFig)
@@ -31,28 +55,26 @@ function getSimplifiedPeople(peopleList) {
         return {
             id: person.id,
             name: `${person.first_name}${middleName} ${person.last_name}`,
-            imgURL: `https://www.govtrack.us/static/legislator-photos/${person.govtrack_id}-100px.jpeg`
+            imgURL: `https://www.govtrack.us/static/legislator-photos/${person.govtrack_id}-100px.jpeg`,
+            seniority: parseInt(person.seniority, 10),
+            party: person.party,
+            missed_votes_pct: person.missed_votes_pct
         }
     })
 }
 
-populateCongressDiv(getSimplifiedPeople(representatives))
+function senioritySort() {
+    populateCongressDiv(getSimplifiedPeople(senators).sort((a, b) => a.seniority - b.seniority).reverse())
+}
 
-/*const repubButton = document.querySelector('#republicans')
+const filterCongressPeople = (chamber, politicalParty) => {
+    return getSimplifiedPeople(chamber).filter(member => member.party === politicalParty)
+}
 
-repubButton.addEventListener('click', () => {
-    showRepublicans()
-})
+const missedVotesMember = (chamber) => {
+    const highestMissedVotesPerson = getSimplifiedPeople(chamber).reduce((acc, member) => acc.missed_votes_pct > member.missed_votes_pct ? acc : member)
+    return getSimplifiedPeople(chamber).filter((person) => person.missed_votes_pct === highestMissedVotesPerson.missed_votes_pct)
+}
 
-function showRepublicans() {
-    //const repubs = representatives.filter(rep => rep.party === 'R')
-    const repubs = representatives.map(rep => {
-        let smallRepub = {}
-    if (rep.party === 'R') {
-        smallRepub.id = rep.id,
-            smallRepub.name = `${rep.first_name} ${rep.middle_name} ${rep.last_name}`
-        }
-        return smallRepub
-})
-    console.log(repubs)
-}*/
+populateCongressDiv(getSimplifiedPeople(senators))
+
